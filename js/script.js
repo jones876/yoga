421d07abfd439a4ae65acd3150018f142a48635f
jones876 <jones111080@yandex.ru>
2020-08-15 12:22:47 +0300

    add modal2

--- js/script.js
+++ js/script.js
@@ -93,7 +93,23 @@ setClock('timer', deadline);
 
 let more = document.querySelector('.more'),
     overlay = document.querySelector('.overlay'),
-    close = document.querySelector('.popup-close');
+    close = document.querySelector('.popup-close'),
+    descr = document.querySelectorAll('.description-btn');
+
+    for(let a=0;a<descr.length;a++){
+        descr[a].addEventListener('click', function(){
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+        
+        close.addEventListener('click', function(){
+            overlay.style.display = 'none';
+            descr.classList.remove('more-splash');
+            document.body.style.overflow = '';
+        });
+       }
+
 
     more.addEventListener('click', function(){
         overlay.style.display = 'block';
